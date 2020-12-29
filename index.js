c38e46e5ae4535d9799ba54cea4f4f4a1380f6da
Parker Strand <parker.strand@verizon.net>
2020-12-29 19:48:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,21 +7,3 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return array;
 }
-
-function johnLennonFacts() {
-  //var facts = [
-// "He was the last Beatle to learn to drive",
-//  "He was never a vegetarian",
-//  "He was a choir boy and boy scout",
-//  "He hated the sound of his own voice"
-//];
-
-// function johnLennonFacts(facts) {
-  // const arr=[];
-  // let i = 0;
-  // while(facts.length > i){
-   //  arr.push(`${facts[i]}!!!`)
-    // i++
-  // }
-   //return arr
- //}
